1c2740dd0f295bf8c95dc82c43bd3ba3e8e38f22
KalesAndRancor <some___guy@hotmail.com>
2020-04-02 10:05:05 -0500

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+iSpy: Addon for automatically targetting NPCs like chests in Odyssey and Lamps in Nyzul.
+
+Commands:
+
+//spy : Turns spying on and off.
+//ispy : Turns spying on and off.
+//ispy report: Turns iSpy reporting found targets to party on and off.
+//ispy Odyssey: Looks for chests in Odyssey.
+//ispy Nyzul: Looks for Runic Lamps in Nyzul.
+//ispy questions: Makes it looks for ??? as found in many places.
---
+++ iSpy.lua
@@ -0,0 +1,74 @@
+_addon.name     = 'iSpy'
+_addon.author   = 'Selindrile'
+_addon.version  = '1.0'
+_addon.commands = {'spy','ispy'}
+
+require('luau')
+require('coroutine')
+packets = require('packets')
+res = require('resources')
+
+spy = true
+setting = 'odyssey'
+report = false
+lastreported = nil
+
+SpiedMobs = {
+	odyssey = S{'Chest'},
+	nyzul = S{'Runic Lamp'},
+	questions = S{'???'},
+	}
+
+windower.register_event('addon command',function (...)
+    cmd = {...}
+
+	if cmd[1] ~= nil then
+		cmd[1] = cmd[1]:lower()
+	end
+	
+	if cmd[1] == nil then
+		if spy == true then
+			spy = false
+			windower.add_to_chat(7,'Spying is [OFF].')
+		else
+			spy = true
+			windower.add_to_chat(7,'Spying is [ON].')
+		end
+	elseif cmd[1] == 'report' then
+		if report == true then
+			report = false
+			windower.add_to_chat(7,'Reporting is [OFF].')
+		else
+			report = true
+			windower.add_to_chat(7,'Reporting is [ON].')
+		end
+	elseif SpiedMobs[cmd[1]] then
+		windower.add_to_chat(7,'Spied mobs set to '..cmd[1]..'.')
+		setting = cmd[1]
+    end
+
+end)
+
+function Spy()
+	if spy then
+		local mobs = windower.ffxi.get_mob_array()
+		for i, mob in pairs(mobs) do
+			if SpiedMobs[setting]:contains(mob.name) and (math.sqrt(mob.distance) < 50) then
+				local player = windower.ffxi.get_player()
+				if player.target_index == nil then
+					packets.inject(packets.new('incoming', 0x058, {
+						['Player'] = player.id,
+						['Target'] = mob.id,
+						['Player Index'] = player.index,
+					}))
+					if report and mob.id ~= lastreported then
+						windower.chat.input('/p Found ['..mob.name..'] at <pos>!')
+						lastreported = mob.id
+					end
+				end
+			end
+		end
+	end
+end
+
+Spy:loop(2)
